9ffd9aefbf0ccbdf202b0ac73fce13ff174c96bc
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-07-24 15:30:36 +0300

    Extending Styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
-import StyledButton from "./components/Button";
-
+import StyledButton, { FancyButton } from "./components/Button";
 
 function App() {
 	return (
 		<div>
-			
 			<StyledButton>Styled button</StyledButton>
-			<div><br/></div>
-			<StyledButton variant='outline'>Styled button</StyledButton>
+			<div>
+				<br />
+			</div>
+			<StyledButton variant="outline">Styled button</StyledButton>
+			<div>
+				<br />
+			</div>
+			<FancyButton as="a">Fancy Button</FancyButton>
 		</div>
 	);
 }
--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const StyledButton = styled.button`
-	background-color: ${(props) => (props.variant === 'outline' ? '#fff' : "#4caf50")};
+	background-color: ${(props) =>
+		props.variant === "outline" ? "#fff" : "#4caf50"};
 	border: 2px solid #4caf50;
-	color: ${(props) => (props.variant === 'outline' ? "#4caf50" : "#fff")};
+	color: ${(props) => (props.variant === "outline" ? "#4caf50" : "#fff")};
 	padding: 15px 32px;
 	text-align: center;
 	text-decoration: none;
@@ -11,4 +12,9 @@ export const StyledButton = styled.button`
 	cursor: pointer;
 	font-size: 16px;
 	transition: 0.5s all ease-out;
+`;
+
+export const FancyButton = styled(StyledButton)`
+	background-image: linear-gradient(to right, #f6d365 0%, #fda085 100%);
+	border: none;
 `;
