b131b1d211335f844ca5b080ec8f1377068dd456
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-07-24 11:35:08 +0300

    Basic Styled Component

---
+++ src/components/Button/Button.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+	background-color: #4caf50;
+	border: none;
+	color: white;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	cursor: pointer;
+	font-size: 16px;
+	transition: 0.5s all ease-out;
+`;
